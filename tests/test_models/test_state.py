52fab12a18a9a8e361df855cee844423793a166b
SrDri <juanjcarabali@gmail.com>
2021-07-01 04:39:32 +0700

    Test state - unittest fix

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
-""" Unittest Place """
+""" Unittest State """
 import unittest
 import os
 import pep8
-from models.place import Place
+from models.state import State
 from models.base_model import BaseModel
 
 
-class TestPlace(unittest.TestCase):
+class TestState(unittest.TestCase):
     """ Testing """
 
     @classmethod
     def setUp(cls):
-        cls.my_place = Place()
-        cls.my_place.name = "Juan"
+        cls.my_state = State()
+        cls.my_state.name = "Juan"
 
-    def test_place_pep8(self):
+    def test_state_pep8(self):
         """ tests pep8 """
         estilo = pep8.StyleGuide(quiet=True)
-        path = estilo.check_files(['models/place.py'])
+        path = estilo.check_files(['models/state.py'])
         self.assertEqual(path.total_errors, 0, "fix pep8")
 
-    def test_place_subclass(self):
-        self.assertTrue(issubclass(self.my_place.__class__, BaseModel), True)
+    def test_state_subclass(self):
+        self.assertTrue(issubclass(self.my_state.__class__, BaseModel), True)
 
-    def test_place_doc(self):
-        self.assertIsNotNone(Place.__doc__)
+    def test_state_doc(self):
+        self.assertIsNotNone(State.__doc__)
 
-    def test_place_atr(self):
-        self.assertTrue('name' in self.my_place.__dict__)
-        self.assertTrue('created_at' in self.my_place.__dict__)
-        self.assertTrue('updated_at' in self.my_place.__dict__)
+    def test_state_atr(self):
+        self.assertTrue('name' in self.my_state.__dict__)
+        self.assertTrue('created_at' in self.my_state.__dict__)
+        self.assertTrue('updated_at' in self.my_state.__dict__)
 
-    def test_place_save(self):
-        self.my_place.save()
-        created = self.my_place.created_at
-        updated = self.my_place.updated_at
+    def test_state_save(self):
+        self.my_state.save()
+        created = self.my_state.created_at
+        updated = self.my_state.updated_at
         self.assertNotEqual(created, updated)
 
-    def test_place_todict(self):
-        self.assertEqual('to_dict' in dir(self.my_place), True)
+    def test_state_todict(self):
+        self.assertEqual('to_dict' in dir(self.my_state), True)
 
 if __name__ == "__main__":
     unittest.main()
